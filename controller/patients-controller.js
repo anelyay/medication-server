ee990f7cb2874521c9662c0d55e8452c1a789c51
AanelyaA <yerzhanova.anelya@gmail.com>
2024-06-03 19:46:17 -0400

    added add, update patient methods

--- controller/patients-controller.js
+++ controller/patients-controller.js
@@ -3,10 +3,18 @@ const knex = require("knex")(require("../knexfile"));
 const selectPatientFields = () => [
   "id",
   "patient_name",
-  "patient_dob",
+  knex.raw("DATE_FORMAT(patient_dob, '%Y-%m-%d') AS patient_dob"),
   "patient_allergy",
   "patient_md",
 ];
+
+const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  return `${year}-${month}-${day}`;
+};
 
 //get all patients
 const index = async (_req, res) => {
@@ -57,17 +65,91 @@ const remove = async (req, res) => {
         .json({ message: `Patient with ID ${req.params.id} not found` });
     }
 
-    res
-      .sendStatus(204);
+    res.sendStatus(204);
   } catch (error) {
     res.status(500).json({ message: `Unable to delete patient; ${error}` });
   }
 };
 
+// add a patient
 
+const add = async (req, res) => {
+  const { patient_name, patient_dob, patient_allergy, patient_md } = req.body;
+
+  const errors = [];
+
+  if (!patient_name) errors.push({ msg: "Patient name is required" });
+  if (!patient_dob) errors.push({ msg: "Date of Birth is required" });
+  if (!patient_md) errors.push({ msg: "Primary Doctor is required" });
+
+  if (errors.length > 0) {
+    return res.status(400).json({ errors });
+  }
+
+  try {
+    const formattedPatientDob = formatDate(patient_dob);
+
+    const result = await knex("patients").insert({
+      patient_name,
+      patient_dob: formattedPatientDob,
+      patient_allergy,
+      patient_md,
+    });
+
+    const newPatientId = result[0];
+
+    const newPatient = await knex("patients")
+      .where({ id: newPatientId })
+      .select(selectPatientFields())
+      .first();
+
+    res.status(201).json(newPatient);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to create a new patient" });
+  }
+};
+
+//update a patient
+const update = async (req, res) => {
+  const { patient_name, patient_dob, patient_allergy, patient_md } = req.body;
+
+  const errors = [];
+  if (!patient_name) errors.push({ msg: "Patient name is required" });
+  if (!patient_dob) errors.push({ msg: "Date of Birth is required" });
+  if (!patient_md) errors.push({ msg: "Primary Doctor is required" });
+
+  if (errors.length > 0) {
+    return res.status(400).json({ errors });
+  }
+
+  try {
+    const rowsUpdated = await knex("patients")
+      .where({ id: req.params.id })
+      .update({ patient_name, patient_dob, patient_allergy, patient_md });
+
+    if (rowsUpdated === 0) {
+      return res.status(404).json({
+        message: `Patient with ID ${req.params.id} not found`,
+      });
+    }
+
+    const updatedPatient = await knex("patients")
+      .select(selectPatientFields())
+      .where({ id: req.params.id })
+      .first();
+
+    res.json(updatedPatient);
+  } catch (error) {
+    res.status(500).json({
+      message: `Unable to update warehouse with ID ${req.params.id}`,
+    });
+  }
+};
 
 module.exports = {
   index,
   findOne,
   remove,
+  add,
+  update
 };
--- routes/patient-route.js
+++ routes/patient-route.js
@@ -2,14 +2,15 @@ const express = require("express");
 const router = express.Router();
 const patientsController = require("../controller/patients-controller");
 
-router.route("/").get(patientsController.index);
-// .post(patientsController.add);
+router.route("/")
+.get(patientsController.index)
+.post(patientsController.add);
 
 router
   .route("/:id")
   .get(patientsController.findOne)
-  .delete(patientsController.remove);
-//   .put(patientsController.update);
+  .delete(patientsController.remove)
+  .put(patientsController.update);
 
 router.route("/:id/medications");
 // .get(patientsController.findAllForOne);
