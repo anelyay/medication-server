3062ae5ddb1befe22ee70c25ce336e9a8af793e4
AanelyaA <yerzhanova.anelya@gmail.com>
2024-06-03 18:13:15 -0400

    added more of initial files, server is working

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.send(`<h1>Welcome to my Server</h1>`);
+});
+
+// // Routes
+// const patientsRouter = require("./routes/patients");
+// const medicationsRouter = require("./routes/medications");
+// app.use("/patients", patientsRouter);
+// app.use("/medications", medicationsRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
