4f3926c4a694acca7d964df78de1ca42368af1c2
AanelyaA <yerzhanova.anelya@gmail.com>
2024-06-03 20:14:59 -0400

    created medication find all/one methods

--- controller/medications-controller.js
+++ controller/medications-controller.js
@@ -1,1 +1,72 @@
 const knex = require("knex")(require("../knexfile"));
+
+//find all
+const findAll = async (_req, res) => {
+  try {
+    const medications = await knex("medications")
+      .join("patients", "medications.patient_id", "patients.id")
+      .select(
+        "medications.id as id",
+        "patients.patient_name",
+        "med_name",
+        "med_dose",
+        "med_schedule",
+        "med_time",
+        "med_taken",
+        "quantity"
+      );
+
+    const formattedMedications = medications.map((pill) => ({
+      ...pill,
+      med_taken: pill.med_taken === 1,
+    }));
+
+    res.status(200).json(formattedMedications);
+  } catch (error) {
+    res.status(500).json({ message: `Error retrieving medications: ${error}` });
+  }
+};
+
+//find one medication
+const findOne = async (req, res) => {
+  try {
+    const medicationId = req.params.id;
+
+    const medication = await knex("medications")
+      .join("patients", "medications.patient_id", "patients.id")
+      .select(
+        "medications.id as id",
+        "patients.patient_name",
+        "med_name",
+        "med_dose",
+        "med_schedule",
+        "med_time",
+        "med_taken",
+        "quantity"
+      )
+      .where("medications.id", medicationId)
+      .first();
+
+    if (!medication) {
+      return res.status(404).json({
+        message: `medication with ID ${medicationId} not found`,
+      });
+    }
+
+    const formattedMedication = {
+      ...medication,
+      med_taken: medication.med_taken === 1,
+    };
+
+    res.status(200).json(formattedMedication);
+  } catch (error) {
+    res.status(500).json({
+      message: `Unable to retrieve medication with ID ${medicationId}`,
+    });
+  }
+};
+
+module.exports = {
+  findAll,
+  findOne,
+};
--- routes/medication-route.js
+++ routes/medication-route.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 const medicationController = require("../controller/medications-controller");
 
-router.route("/");
+router.route("/").get(medicationController.findAll);
 //   .post(medicationController.add);
 
-router.route("/:id");
-//   .get(medicationController.findOne)
+router.route("/:id")
+  .get(medicationController.findOne)
 //   .delete(medicationController.remove)
 //   .put(medicationController.update);
 
