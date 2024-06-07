ebe0738e932f04eb9a3e7ddf84c3807ced8e79d5
AanelyaA <yerzhanova.anelya@gmail.com>
2024-06-07 10:07:26 -0400

    added text-recognition functionality

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const multer = require ("multer");
+const tesseract = require("node-tesseract-ocr");
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -10,6 +12,31 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const upload = multer({ dest: "backend/uploads/" });
+
+app.post("/files", upload.single("screenshots"), (req, res) => {
+  // return received files info back to frontend
+  res.json(req.file.originalname);
+  console.log(req.file);
+
+  const config = {
+    lang: "eng",
+    oem: 1,
+    psm: 3,
+  };
+
+  tesseract
+    .recognize(req.file.path, config)
+    .then((text) => {
+      ///////// text PRINT RESULT OF RECOGNITION ////////////
+      console.log("Text:", text);
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+});
 
 app.get("/", (_req, res) => {
   res.send(`<h1>Welcome to my Server</h1>`);
