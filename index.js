3b63cfca970b6923d32000ceec53fa177b7ee004
AanelyaA <yerzhanova.anelya@gmail.com>
2024-06-19 22:09:41 -0400

    added authentication route and a controller

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+// const jwt = require("jsonwebtoken");
+
+const patientsRouter = require("./routes/patient-route");
+const medicationsRouter = require("./routes/medication-route");
+// const authRoutes = require("./routes/auth");
+
 dotenv.config();
 
 const PORT = process.env.PORT || 8080;
@@ -11,10 +17,24 @@ app.use(cors());
 app.use(express.json());
 
 
-const patientsRouter = require("./routes/patient-route");
-const medicationsRouter = require("./routes/medication-route");
+// app.use("/auth", authRoutes);
+
+// function authenticateToken(req, res, next) {
+//   const authHeader = req.headers["authorization"];
+//   const token = authHeader && authHeader.split(" ")[1];
+//   if (token == null) return res.sendStatus(401);
+
+//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+//     if (err) return res.sendStatus(403);
+//     req.user = user;
+//     next();
+//   });
+// }
+
 app.use("/patients", patientsRouter);
 app.use("/medications", medicationsRouter);
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
