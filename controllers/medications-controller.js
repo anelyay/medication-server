3f3026b1100e196b08a5bab896d305c8d635bd98
AanelyaA <yerzhanova.anelya@gmail.com>
2024-06-23 13:44:07 -0400

    changed cron fx to update at midnight any local time

--- controllers/medications-controller.js
+++ controllers/medications-controller.js
@@ -438,7 +438,7 @@ const markMedicationAsTakenWithNFC = async (req, res) => {
   }
 };
 
-cron.schedule("0 4 * * *", async () => {
+cron.schedule("0 0 * * *", async () => {
   try {
     await knex("schedule").update({ med_taken: false });
   } catch (error) {
