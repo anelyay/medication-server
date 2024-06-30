7784bf5769163d3a0ff041b79f37c581b25c98be
AanelyaA <yerzhanova.anelya@gmail.com>
2024-06-29 21:37:26 -0400

    fixed a const

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -221,7 +221,7 @@ const refresher = async (req, res) => {
     }
 
     // Query all medications and med_taken status related to user.id
-        const medication_status = await knex("schedule")
+        const medications = await knex("schedule")
            .select("id", "medication_id", "med_taken")
            .where({ user_id: user.id });
 
