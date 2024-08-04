8f3e6c84fbb1b36db9b67bda1d7e66dfe63dc3c6
AanelyaA <yerzhanova.anelya@gmail.com>
2024-08-03 20:28:34 -0400

    edited the route for nfc on index

--- routes/nfc.js
+++ routes/nfc.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const nfcController = require("../controllers/nfc-controller");
+const nfcController = require("../controllers/nfc-conroller");
 
 
 // router.route("/").post(nfcController);
